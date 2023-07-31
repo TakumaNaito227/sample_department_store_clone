286b9ba65c343ad39f86e2c1f824bf4d1b654e8a
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2023-07-31 22:05:51 +0900

    footer実装

--- lib/footer.dart
+++ lib/footer.dart
@@ -1,27 +1,10 @@
-// import 'package:flutter/material.dart';
-//
-// class Footer extends StatelessWidget {
-//   const Footer({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return BottomNavigationBar(
-//       items: const [
-//         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
-//         BottomNavigationBarItem(icon: Icon(Icons.search), label: '検索'),
-//         BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'お気に入り'),
-//         BottomNavigationBarItem(icon: Icon(Icons.hexagon), label: '特集'),
-//         BottomNavigationBarItem(icon: Icon(Icons.person), label: 'マイページ'),
-//       ],
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 
 class Footer extends StatefulWidget {
-  const Footer();
+  const Footer({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _Footer createState() => _Footer();
 }
 
@@ -29,16 +12,14 @@ class _Footer extends State {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home2',
-        ),
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: '検索'),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'お気に入り'),
+        BottomNavigationBarItem(icon: Icon(Icons.hexagon), label: '特集'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'マイページ'),
       ],
+      type: BottomNavigationBarType.fixed,
     );
   }
 }
