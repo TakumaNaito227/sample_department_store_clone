b0981b21ca5188c320e67ff51ba208cae92915c4
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2023-08-09 18:49:21 +0900

    firebase 連携

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:applovin_max/applovin_max.dart';
 import 'package:flutter/material.dart';
 
 import 'footer.dart';
@@ -12,17 +13,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const Scaffold(
-        body: Home(),
-        bottomNavigationBar: Footer(),
-      ),
+    return FutureBuilder<Map?>(
+      future: initializeAppLovinSdk(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return const MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Error',
+              home: Scaffold(
+                body: Center(
+                  child: Text(
+                      'An error occurred while initializing the AppLovin SDK.'),
+                ),
+              ),
+            );
+          } else {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+                visualDensity: VisualDensity.adaptivePlatformDensity,
+              ),
+              home: const Scaffold(
+                body: Home(),
+                bottomNavigationBar: Footer(),
+              ),
+            );
+          }
+        } else {
+          return const MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Loading',
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        }
+      },
     );
+  }
+
+  Future<Map?> initializeAppLovinSdk() async {
+    try {
+      return await AppLovinMAX.initialize("YOUR_SDK_KEY_HERE");
+    } catch (error) {
+      print("Error initializing AppLovin SDK: $error");
+      return null;
+    }
   }
 }
